a4ee8a86040ecc59689aead3d1e90db4e96f9c14
Luan <bonomiluan@gmail.com>
2018-08-15 09:01:03 -0300

    criado projeto maven

---
+++ compilador/src/main/java/com/puc/compilador/compilador/CompiladorApplication.java
@@ -0,0 +1,12 @@
+package com.puc.compilador.compilador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompiladorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompiladorApplication.class, args);
+	}
+}
